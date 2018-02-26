3c11055f5dd3fadd0bd95ec47e037ae82e82fe28
张海彪 <1473209216@qq.com>
2018-02-26 15:33:00 +0800

    添加StreamGobbler类

---
+++ src/main/java/com/biubiu/StreamGobbler.java
@@ -0,0 +1,23 @@
+/**
+ * Created by zhanghaibiao on 2018/1/21.
+ */
+public class StreamGobbler {
+
+    private InputStream inputStream;
+
+    private Consumer<String> consumer;
+
+    public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+        this.inputStream = inputStream;
+        this.consumer = consumer;
+    }
+
+    public void run() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            reader.lines().forEach(consumer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
