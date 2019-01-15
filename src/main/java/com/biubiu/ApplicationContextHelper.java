592a4af5ae199eba96b79e3b7014f0b8f91a497e
wuzuilanhai <1473209216@qq.com>
2019-01-15 14:25:46 +0800

    工具类修改

---
+++ src/main/java/com/biubiu/ApplicationContextHelper.java
@@ -0,0 +1,102 @@
+package com.biubiu;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by Haibiao.Zhang on 2018/8/27.
+ */
+public class ApplicationContextHelper implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        ApplicationContextHelper.applicationContext = applicationContext;
+    }
+
+    /**
+     * 获取请求客户端ip
+     *
+     * @return 客户端ip
+     */
+    public static String getRequestIp() {
+        HttpServletRequest request = getRequest();
+        String ip = request.getHeader("X-Forwarded-For");
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+    /**
+     * 获取HttpServletRequest对象
+     *
+     * @return HttpServletRequest对象
+     */
+    public static HttpServletRequest getRequest() {
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+        return sra.getRequest();
+    }
+
+    /**
+     * 获取HttpServletResponse对象
+     *
+     * @return HttpServletResponse对象
+     */
+    public static HttpServletResponse getResponse() {
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+        return sra.getResponse();
+    }
+
+    /**
+     * 获取HttpSession对象
+     *
+     * @return HttpSession对象
+     */
+    public static HttpSession getSession() {
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+        return sra.getRequest().getSession();
+    }
+
+    /**
+     * 获取bean
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        return applicationContext.getBean(clazz);
+    }
+
+    /**
+     * 获取bean
+     */
+    public static <T> T getBean(String beanName, Class<T> clazz) {
+        return applicationContext.getBean(beanName, clazz);
+    }
+
+}
